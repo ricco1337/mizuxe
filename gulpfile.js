3820faf5062c5a247ecc8ecac3cb47588bfdefd1
ricco1337 <ricco1337@outlook.com>
2018-02-05 20:57:22 -0800

    moved gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,6 @@ gulp.task('sass', function(){
     .pipe(browserSync.stream());
 });
 
-gulp.task('deploy', function() {
-  return gulp.src('./index.html/**/*')
-    .pipe(ghPages());
-});
-
 // Move JS Files to src/js
 gulp.task('js', function(){
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
@@ -46,4 +41,8 @@ gulp.task('fa', function(){
     .pipe(gulp.dest("src/css"));
 });
 
+gulp.task('deploy', function() {
+  return gulp.src('./index.html/**/*')
+    .pipe(ghPages());
+});
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
