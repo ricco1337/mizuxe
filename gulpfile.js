7d6a8b78b98e5707e44611b26e06a06ff9ba1e33
ricco1337 <ricco1337@outlook.com>
2018-02-06 00:30:19 -0800

    moved index & gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,7 @@ gulp.task('js', function(){
     .pipe(browserSync.stream());
 });
 
-gulp.task('deploy', function() {
-  return gulp.src('index.html')
-    .pipe(ghPages());
-});
+
 // Watch Sass & Server
 gulp.task('serve', ['sass'], function(){
   browserSync.init({
@@ -46,5 +43,8 @@ gulp.task('fa', function(){
     .pipe(gulp.dest("src/css"));
 });
 
-
+gulp.task('deploy', function() {
+  return gulp.src('index.html')
+    .pipe(ghPages());
+});
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
