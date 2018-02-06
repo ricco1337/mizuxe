baa06717b0aa764ce03dba2da4c057257808ba3e
ricco1337 <ricco1337@outlook.com>
2018-02-05 20:43:55 -0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,14 @@ gulp.task('sass', function(){
     .pipe(browserSync.stream());
 });
 
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
 // Move JS Files to src/js
 gulp.task('js', function(){
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
